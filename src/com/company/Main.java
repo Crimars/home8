7caf7a73bc1a38e314c5c3ec8860fc0cab0947d6
Crimars_work <isogd.kr@gmail.com>
2023-02-06 15:41:07 +0300

    calculator in work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,29 +3,56 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        char operation = sc.next().charAt(0);
-        int num2 = sc.nextInt();
 
-
-        if (operation == '+') {
-            System.out.println(num1 + " + "  + num2 + " = " + (num1 + num2));}
-        else if (operation == '-'){
-            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));}
-        else if (operation == '*'){
-            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));}
-        else if (operation == '/'){
-            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));}
-        else if (operation == '/' && num2 == 0){
-            System.out.println("на ноль делить нельзя");
-        }
-        else{
-            System.out.println("неверная операция");
-        }
-
-
+        int num1 = nInt();
+        int num2 = nInt();
+        char operation = operat();
     }
-}
+
+    public static int nInt() {
+        if (sc.hasNextInt()) {
+            int num1 = sc.nextInt();
+            System.out.println("ввели число " + num1);
+        } else {
+            System.out.println("это не число!");
+        }
+        return 0;
+    }
+
+    public static char operat() {
+        if (sc.hasNext()) {
+            char operation = sc.next().charAt(0);
+        } else {
+            System.out.println("не операция");
+            char operation = operat();
+        }
+        return 0;
+    }
+
+
+
+
+
+            public static int calc(int num1, int num2, char operation){
+
+                if (operation == '+') {
+                    System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                } else if (operation == '-') {
+                    System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                } else if (operation == '*') {
+                    System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                } else if (operation == '/') {
+                    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                } else {
+                    System.out.println("неверная операция");
+                }
+
+return num1;
+            }
+        }
+
+
+
